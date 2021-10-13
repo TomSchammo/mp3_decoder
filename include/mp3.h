077093e7846c3c320f9b692574ff807cc4523c5b
Tom Schammo <tom@tomschammo.com>
2021-10-14 01:23:56 +0200

    Added documentation
    Added documentation to calculate_frame_length and read_header

--- include/mp3.h
+++ include/mp3.h
@@ -25,8 +25,28 @@ typedef struct {
 } mp3_container;
 
 
+
+/**
+ * Reads the frame header and sets the information in the mp3 container
+ *
+ * @param position is the position of the start of the frame
+ * @param mp3      contains various information necessary to deal with the data
+ *
+ * @return 0 if the function terminates successfully, -1 if it terminates on a failure
+ */
 int read_header(uint64_t position, mp3_container mp3);
 
+
+/**
+ * Calculates the length of the current frame in bytes.
+ * With the 4 bytes of the header included.
+ *
+ * @param bit_rate    is the bitrate specified in the header for the following data
+ * @param sample_rate is the sampling rate specified in the header for the following data
+ * @param padding     indicates whether there is padding added. (1 if there is padding, 0 otherwise)
+ *
+ * @return the length of the current frame in bytes
+ */
 uint32_t calculate_frame_length(uint32_t bit_rate, uint32_t sample_rate, byte padding);
 
 #endif // MP3_H
